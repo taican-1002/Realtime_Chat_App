55684c66babe94b6c16c75b33a8bb44bf55f3566
Tài Cẩn <taicancr7@gmail.com>
2022-08-04 11:01:00 +0700

    init commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./src/routes/userRoutes");
+
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", userRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Successfull");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+const server = app.listen(process.env.PORT, () => {
+  console.log("Server start on port:", process.env.PORT);
+});
